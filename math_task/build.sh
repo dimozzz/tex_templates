bd38229f9cdb562733bea06aaa6cd4558aa250f0
dimozzz <dimoz_88@rambler.ru>
2012-05-09 14:48:24 +0400

    1

--- math_task/build.sh
+++ math_task/build.sh
@@ -1,3 +1,19 @@
 mkdir tmp
-pdflatex --output-directory tmp main.tex
-pdflatex --output-directory tmp main.tex
+for i in *; do
+	if typeset -i "$i" 2> /dev/null; then
+		echo -n
+	else
+		if test -d "$i"; then
+			cp main.tex "$i"
+			cd "$i"
+			pdflatex --output-directory ../tmp -jobname "$i" main.tex
+			pdflatex --output-directory ../tmp -jobname "$i" main.tex
+			rm main.tex
+			cd ..	
+		fi
+	fi
+done
+
+cd tmp
+rm *.log
+rm *.aux
